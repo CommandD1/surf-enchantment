792889a1aa5ec92c1b84c50502bbb2ae8f2ed636
CommandD1 <101664873+CommandD1@users.noreply.github.com>
2026-04-07 09:15:33 +0200

    Add new item type tags for bows, crossbows, and mace
    Add missing weapons to be telekinesis enchanted.

--- surf-enchantment-api/src/main/kotlin/dev/slne/surf/enchantment/api/utils/CustomItemTypeTags.kt
+++ surf-enchantment-api/src/main/kotlin/dev/slne/surf/enchantment/api/utils/CustomItemTypeTags.kt
@@ -81,7 +81,11 @@ enum class CustomItemTypeTags(
         TagEntry.tagEntry(ItemTypeTagKeys.PICKAXES),
         TagEntry.tagEntry(ItemTypeTagKeys.SHOVELS),
         TagEntry.tagEntry(ItemTypeTagKeys.SWORDS),
-        TagEntry.valueEntry(ItemTypeKeys.TRIDENT)
+        TagEntry.valueEntry(ItemTypeKeys.TRIDENT),
+        TagEntry.tagEntry(ItemTypeKeys.BOWS),
+        TagEntry.tagEntry(ItemTypeKeys.CROSSBOWS),
+        TagEntry.tagEntry(ItemTypeKeys.SPEARS),
+        TagEntry.valueEntry(ItemTypeKeys.MACE)
     ),
 
     VEIN_MINER_KEY(
